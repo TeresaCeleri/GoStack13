bafd02ba6c105ab2fcf6338f0374b43d32b76f6a
TeresaCeleri <teresaceleri18@gmail.com>
2020-08-31 22:41:44 -0300

    Aula4-model_agendamento

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
-import express, { request, response } from "express";
+import express from "express";
+import routes from './routes';
 
 const app = express();
 
-app.get("/", (request, response) => {
-  return response.json({ message: "Hello Gostack13" });
-});
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333, () => {
-  console.log("server started on port 3333");
+  console.log("Server started on port 3333");
 });
