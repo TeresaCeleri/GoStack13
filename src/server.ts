1f33447e933b88f72d3e71151e25a58ec1d98814
TeresaCeleri <teresaceleri18@gmail.com>
2020-10-04 16:18:49 -0300

    backend_com_erro

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import 'reflect-metadata';
 
 import express, { Request, Response, NextFunction } from "express";
 
+import cors from 'cors';
+
 import 'express-async-errors';
 
 import routes from './routes';
@@ -12,27 +14,26 @@ import './database';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
-app.use(
-  (err: Error, request: Request, response: Response, next: NextFunction) => {
-    if (err instanceof AppError)
-      return response.status(err.statusCode).json({
-        status: 'error',
-        message: err.message,
-      });
-    }
-
-    console.error(err);
-    
-    return response.status(500).json({
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+  if (err instanceof AppError)
+    return response.status(err.statusCode).json({
       status: 'error',
-      message: 'Internal server error',
+      message: err.message,
     });
-  },
-);
+  }
+
+ console.error(err);
+  
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
 
 app.listen(3333, () => {
   console.log("Server started on port 3333");
