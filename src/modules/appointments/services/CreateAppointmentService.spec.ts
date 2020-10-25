3ae111c7ab1f8a69137aecd9bad47dcb62b94fd3
TeresaCeleri <teresaceleri18@gmail.com>
2020-10-25 17:39:59 -0300

    backend_N4_aula15

---
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -0,0 +1,15 @@
+// test('dois numeros', () => {
+//     expect(1 + 2).toBe(3);
+// });
+
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+  it('Should be able to create a new appointment', () => {
+    expect(1 + 2).toBe(3);
+  });
+
+  // it('Should Not be able to create two appointments on the same', () => {
+  //   expect(1 + 2).toBe(3);
+  // });
+});
--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -4,9 +4,9 @@ import express, { Request, Response, NextFunction } from "express";
 import cors from 'cors';
 import 'express-async-errors';
 
+import routes from './routes';
 import uploadConfig from '@config/upload';
 import AppError from '@shared/errors/AppError';
-import routes from './routes';
 
 import '@shared/infra/typeorm';
 import '@shared/container';
@@ -19,18 +19,18 @@ app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
-  if (err instanceof AppError)
+  if (err instanceof AppError) {    
     return response.status(err.statusCode).json({
       status: 'error',
       message: err.message,
     });
-}
+  }
 
-console.error(err);
+  console.error(err);
 
-return response.status(500).json({
-  status: 'error',
-  message: 'Internal server error',
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+
 });
-
-
