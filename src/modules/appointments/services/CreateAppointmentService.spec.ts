bcb32a7384c850a4b1df3b64ddb51b5348a71f68
TeresaCeleri <teresaceleri18@gmail.com>
2020-10-27 20:15:17 -0300

    yarn_test_erro

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -2,11 +2,28 @@
 //     expect(1 + 2).toBe(3);
 // });
 
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+
 import CreateAppointmentService from './CreateAppointmentService';
 
 describe('CreateAppointment', () => {
-  it('Should be able to create a new appointment', () => {
-    expect(1 + 2).toBe(3);
+  it('Should be able to create a new appointment', async () => {
+    //passamos repositorio fake
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    //criamos noso server
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    //criamos um novo appointment passando
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: '123123123',
+    });
+
+    //verificar se está retornando o esperado
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('123123123');
   });
 
   // it('Should Not be able to create two appointments on the same', () => {
