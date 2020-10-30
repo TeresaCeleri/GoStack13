1d7c65ff4a5f536bd2987d97d512c02004b53dc2
Teresa Bueno <tbueno@oestesaude.br>
2020-10-30 14:01:30 -0300

    nivel4_aula17_ok

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -4,6 +4,7 @@ import express, { Request, Response, NextFunction } from "express";
 import cors from 'cors';
 import 'express-async-errors';
 
+
 import routes from './routes';
 import uploadConfig from '@config/upload';
 import AppError from '@shared/errors/AppError';
