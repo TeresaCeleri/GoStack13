d635205232985a21787d4ccbd48b354e53b8b5a6
Teresa Bueno <tbueno@oestesaude.br>
2020-11-06 17:34:13 -0300

    nivel4_mod2_reset

--- src/modules/users/services/SendForgotPasswordEmailService.spec.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.spec.ts
@@ -1,23 +1,30 @@
 import AppError from '@shared/errors/AppError';
 
 import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import FakeUserTokensRepository from '../repositories/fakes/FakeUsersTokensRepository';
 import FakeMailProvider from '@shared/container/providers/MailProvider/fakes/FakeMailProvider';
 import SendForgotPasswordEmailService from './SendForgotPasswordEmailService';
 
+let fakeUsersRepository: FakeUsersRepository;
+let fakeUserTokensRepository: FakeUserTokensRepository;
+let fakeMailProvider: FakeMailProvider;
+let sendForgotPasswordEmail: SendForgotPasswordEmailService;
+
 describe('SendForgotPasswordEmail', () => {
-  it('Should be able to recover the password using the email', async () => {
-    //passamos repositorio fake
-    const fakeUsersRepository = new FakeUsersRepository();
-    const fakeMailProvider = new FakeMailProvider();
+  beforeEach(() => {
+    fakeUsersRepository = new FakeUsersRepository();
+    fakeMailProvider = new FakeMailProvider();
+    fakeUserTokensRepository = new FakeUserTokensRepository();
 
-    //verificar se o metodo sendMail foi executado
-    const sendMail = jest.spyOn(fakeMailProvider, 'sendMail');
-
-    //criamos nosso server
-    const sendForgotPasswordEmailService = new SendForgotPasswordEmailService(
+    sendForgotPasswordEmail = new SendForgotPasswordEmailService(
       fakeUsersRepository,
       fakeMailProvider,
+      fakeUserTokensRepository,
     );
+  });
+
+  it('Should be able to recover the password using the email', async () => {
+    const sendMail = jest.spyOn(fakeMailProvider, 'sendMail');
 
     await fakeUsersRepository.create({
       name: 'John Doe',
@@ -26,7 +33,7 @@ describe('SendForgotPasswordEmail', () => {
     });
 
     //criamos um novo appointment passando
-    await sendForgotPasswordEmailService.execute({
+    await sendForgotPasswordEmail.execute({
       email: 'johndoe@exemplo.com',
     });
 
@@ -35,21 +42,27 @@ describe('SendForgotPasswordEmail', () => {
   });
 
   it('Should not to able to recover a non-exiting user password', async () => {
-    //passamos repositorio fake
-    const fakeUsersRepository = new FakeUsersRepository();
-    const fakeMailProvider = new FakeMailProvider();
-
-    //criamos nosso server
-    const sendForgotPasswordEmailService = new SendForgotPasswordEmailService(
-      fakeUsersRepository,
-      fakeMailProvider,
-    );
-
-    //verificar se está retornando o esperado
     await expect(
-      sendForgotPasswordEmailService.execute({
+      sendForgotPasswordEmail.execute({
         email: 'johndoe@exemplo.com',
       }),
     ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should generate a forgot password token', async () => {
+    const generateToken = jest.spyOn(fakeUserTokensRepository, 'generate');
+
+    const user = await fakeUsersRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@exemplo.com',
+      password: '123456',
+    });
+
+    await sendForgotPasswordEmail.execute({
+      email: 'johndoe@exemplo.com',
+    });
+
+    //verificar se está retornando o esperado
+    expect(generateToken).toHaveBeenCalledWith(user.id);
   });
 });
